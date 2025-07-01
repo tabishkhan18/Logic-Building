d7ce75a0c12741cc509500fcb09e8fb7486e9b8a
Tabishkhan18 <tabishkhan1811@gmail.com>
2025-07-01 16:02:22 +0530

    Add function to find the largest single digit in a number

---
+++ LargestDigitInANumber/index.js
@@ -0,0 +1,19 @@
+// Question: Given a number, find the largest single digit in it.
+// 
+
+function largestDigitInANumber(num){
+
+    let n = num;
+    let largest = 0;
+    while(n>0){
+        let rem = n%10
+        if(rem>largest){
+            largest = rem
+        }
+        n = Math.trunc(n/10)
+    }
+    return console.log(largest + " is the largest digit")
+
+}
+
+largestDigitInANumber(451219) // 9 is the largest digit
