c0926fc636c59d7db1681d41a8788bc62c430884
Tabishkhan18 <tabishkhan1811@gmail.com>
2025-08-30 17:33:47 +0530

    Improve readability by adding whitespace in longestPalindrome function

--- LongestPalindromicSubstring/index.js
+++ LongestPalindromicSubstring/index.js
@@ -13,6 +13,7 @@ Explanation: "aba" is also a valid answer.
 function longestPalindrome(s) {
     if (!s || s.length < 1) return "";
     let start = 0, end = 0;
+    
     function expandAroundCenter(left, right) {
         while (left >= 0 && right < s.length && s[left] === s[right]) {
             left--;
@@ -20,9 +21,10 @@ function longestPalindrome(s) {
         }
         return right - left - 1;
     }
+    
     for (let i = 0; i < s.length; i++) {
-        let len1 = expandAroundCenter(i, i);
-        let len2 = expandAroundCenter(i, i + 1);
+        let len1 = expandAroundCenter(i, i); // odd length
+        let len2 = expandAroundCenter(i, i + 1); // for even length
         let len = Math.max(len1, len2);
         if (len > end - start) {
             start = i - Math.floor((len - 1) / 2);
