5b4a3e65de7d9f449243bd52ccb0456465d7a488
Tabishkhan18 <tabishkhan1811@gmail.com>
2025-08-30 20:15:16 +0530

    Refactor palindromicNumber function to improve readability and return boolean value

--- PalindromicNumber/index.js
+++ PalindromicNumber/index.js
@@ -1,3 +1,4 @@
+// #9 Leetcode (Easy)
 // Given a number, check if it is palindromic or not
 // A palindromic number is a number that remains the same when its digits are reversed
 
@@ -9,13 +10,9 @@ function palindromicNumber(n) {
         rev = rev * 10 + rem
         n = Math.floor(n / 10)
     }
-    if(rev === num){
-        console.log("The number is palindromic")
-    }
-    else{
-        console.log("Non palindromic")
-    }
+    if(rev === num) return true
+    else return false
 }
 
-palindromicNumber(12345) // Non palindromic
-palindromicNumber(12321) // The number is palindromic
+console.log(palindromicNumber(12345)) // Non palindromic
+console.log(palindromicNumber(12321)) // The number is palindromic
